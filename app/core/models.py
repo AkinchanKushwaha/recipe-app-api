e59e134afc18a8428b13b2420db5f64071218fb0
Akinchan Kushwaha <akinchankushwaha@Akinchans-Laptop.local>
2024-02-13 13:42:50 +0530

    configured nomalising email and superuser functionality

--- app/core/models.py
+++ app/core/models.py
@@ -15,9 +15,20 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users must have an email address.')
+        user = self.model(**extra_fields, email=self.normalize_email(email))
         user.set_password(password)
         user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
         return user
 
 
