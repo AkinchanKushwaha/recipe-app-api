e06dfe88ad2e2b34ee86ee5e613f38283e3e5a67
Akinchan Kushwaha <akinchankushwaha@Akinchans-Laptop.local>
2024-02-20 15:05:44 +0530

    implemeneted token api

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
-    'user'  
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,4 @@ AUTH_USER_MODEL = 'core.User'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
 }
+
